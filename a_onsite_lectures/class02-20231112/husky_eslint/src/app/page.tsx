31fc065ca0d66492b364afa15de68f1d730f33bc
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-11-10 23:11:13 +0500

    husky_eslint

---
+++ a_onsite_lectures/class02-20231112/husky_eslint/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  // const f_name = "Hassan"
+  // let fName = "Hassan"
+  // const age = "Hassan"
+  // const fName : any = "Hassan"
+  const fName = 'Hassan';
+  return <div>Hello World from {fName}.</div>;
+}
